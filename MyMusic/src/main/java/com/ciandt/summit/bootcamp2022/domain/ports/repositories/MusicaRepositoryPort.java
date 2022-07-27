9164c9ccf7000080f0f1a816f25c046b7fbd666e
tc-anaalves <anaalves@ciandt.com>
2022-07-27 08:48:55 -0300

    feat: criar musica repository port

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/domain/ports/repositories/MusicaRepositoryPort.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/domain/ports/repositories/MusicaRepositoryPort.java
@@ -1,4 +1,13 @@
 package com.ciandt.summit.bootcamp2022.domain.ports.repositories;
 
+import com.ciandt.summit.bootcamp2022.domain.models.Musica;
+
+import java.util.List;
+
 public interface MusicaRepositoryPort {
+
+    List<Musica> findMusicasByFiltro(String filtro);
+
+    List<Musica> findAll();
+
 }
