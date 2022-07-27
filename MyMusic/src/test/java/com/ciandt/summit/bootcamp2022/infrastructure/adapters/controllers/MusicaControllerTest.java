ed916117b5b29a023ca2755984287e072332ae89
CIANDT\igabriel <Igabriel@ciandt.com>
2022-07-26 22:34:00 -0300

    BOOT-355 configurando Junit5

---
+++ MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/infrastructure/adapters/controllers/MusicaControllerTest.java
@@ -0,0 +1,39 @@
+package com.ciandt.summit.bootcamp2022.infrastructure.adapters.controllers;
+
+import com.ciandt.summit.bootcamp2022.domain.ports.interfaces.MusicaServicePort;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.*;
+
+@WebMvcTest
+public class MusicaControllerTest {
+
+    @Autowired
+    private MusicaController musicaController;
+
+    @MockBean
+    private MusicaServicePort musicaServicePortice;
+
+    @BeforeEach
+    public void setup(){
+        RestAssuredMockMvc.standaloneSetup(this.musicaController);
+    }
+
+
+
+    @Test
+    public void deveRetornar200_get(){
+        RestAssuredMockMvc.given()
+                .when()
+                    .get("/api/v1/musicas/")
+                .then()
+                    .statusCode(HttpStatus.SC_OK);
+    }
+
+
+
+}
