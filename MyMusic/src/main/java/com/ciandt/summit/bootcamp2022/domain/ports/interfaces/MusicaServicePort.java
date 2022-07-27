4521a597d33cf3322a94d66909488f8f2dd59da8
tc-anaalves <anaalves@ciandt.com>
2022-07-27 08:51:29 -0300

    feat: criar musica service port

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/domain/ports/interfaces/MusicaServicePort.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/domain/ports/interfaces/MusicaServicePort.java
@@ -1,4 +1,12 @@
 package com.ciandt.summit.bootcamp2022.domain.ports.interfaces;
 
+import com.ciandt.summit.bootcamp2022.domain.dtos.MusicaDTO;
+
+import java.util.List;
+
 public interface MusicaServicePort {
+
+    List<MusicaDTO> findMusicasByFiltro(String filtro);
+    List<MusicaDTO> findAll();
+
 }
