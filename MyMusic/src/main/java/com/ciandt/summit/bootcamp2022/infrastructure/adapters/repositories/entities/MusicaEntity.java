7ebfc57b155e5eff4184711724d67f91eefb91d5
Gabriela Lichy <CIANDT\gabrielalichy@lnb036088cps.ciandt.global>
2022-07-27 16:01:52 -0300

    Adicionando toMusica

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/infrastructure/adapters/repositories/entities/MusicaEntity.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/infrastructure/adapters/repositories/entities/MusicaEntity.java
@@ -1,5 +1,6 @@
 package com.ciandt.summit.bootcamp2022.infrastructure.adapters.repositories.entities;
 
+import com.ciandt.summit.bootcamp2022.domain.models.Musica;
 import lombok.*;
 
 import javax.persistence.*;
@@ -22,13 +23,18 @@ public class MusicaEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY);
     @Column(name = "Id");
-    private String Id;
+    private String id;
 
     @Column(name = "Nome")
-    private String Nome;
+    private String nome;
 
     @ManyToOne
     @JoinColumn(name = "ArtistaId", referencedColumnName = "Id")
     private ArtistaEntity artistaEntity;
+
+
+    public Musica toMusica() {
+        return new Musica(this.id, this.nome, new Artista(artistaEntity.getId(), artistaEntity.getNome()));
+    }
 
 }
