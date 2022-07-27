6c65275792b8db05efdd44ceae966b0c302ebc9e
Cesar A. Pires <cesarp@ciandt.com>
2022-07-27 12:48:47 -0300

    feat: criar interface MusicaJpaRepository

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/infrastructure/adapters/repositories/MusicaJpaRepository.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/infrastructure/adapters/repositories/MusicaJpaRepository.java
@@ -1,4 +1,11 @@
 package com.ciandt.summit.bootcamp2022.infrastructure.adapters.repositories;
 
-public class MusicaJpaRepository {
+import com.ciandt.summit.bootcamp2022.infrastructure.adapters.repositories.entities.Musica;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface MusicaJpaRepository extends JpaRepository<Musica, UUID> {
 }
