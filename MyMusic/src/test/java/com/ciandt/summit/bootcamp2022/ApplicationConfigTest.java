e446142d035345080e184caf71fb9add874d85f0
CIANDT\igabriel <Igabriel@ciandt.com>
2022-07-27 09:58:40 -0300

    BOOT-355 Estruturando classes de testes

---
+++ MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/ApplicationConfigTest.java
@@ -0,0 +1,10 @@
+package com.ciandt.summit.bootcamp2022;
+
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ApplicationConfigTest {
+}
---
+++ MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/domain/services/PlaylistServiceImplTest.java
@@ -0,0 +1,8 @@
+package com.ciandt.summit.bootcamp2022.domain.services;
+
+import com.ciandt.summit.bootcamp2022.ApplicationConfigTest;
+import org.junit.jupiter.api.DisplayName;
+
+@DisplayName("PlaylistServiceImplTest")
+public class PlaylistServiceImplTest extends ApplicationConfigTest {
+}
