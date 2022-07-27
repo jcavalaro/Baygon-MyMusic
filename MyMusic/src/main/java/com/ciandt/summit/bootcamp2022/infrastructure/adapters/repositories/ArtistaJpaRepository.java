671d86fae39a948c0696b29a8da26fe2e51bd13c
Ederson Rafael Nonnemacher <105384086+enonnemacher-ciandt@users.noreply.github.com>
2022-07-27 18:37:18 -0300

    Update ArtistaJpaRepository.java

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/infrastructure/adapters/repositories/ArtistaJpaRepository.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/infrastructure/adapters/repositories/ArtistaJpaRepository.java
@@ -1,9 +1,9 @@
 package com.ciandt.summit.bootcamp2022.infrastructure.adapters.repositories;
 
-import com.ciandt.summit.bootcamp2022.infrastructure.adapters.repositories.entities.Artista;
+import com.ciandt.summit.bootcamp2022.infrastructure.adapters.repositories.entities.ArtistaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ArtistaJpaRepository extends JpaRepository<Artista, String> {
+public interface ArtistaJpaRepository extends JpaRepository<ArtistaEntity, String> {
 }
