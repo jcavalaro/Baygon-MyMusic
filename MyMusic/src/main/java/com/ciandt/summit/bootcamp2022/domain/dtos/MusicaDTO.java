04c9651dcb7faddd4e7832884759267957fbe6b1
Gabriela Lichy <CIANDT\gabrielalichy@lnb036088cps.ciandt.global>
2022-07-28 09:59:19 -0300

    Criar Musica DTO

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/domain/dtos/MusicaDTO.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/domain/dtos/MusicaDTO.java
@@ -1,4 +1,22 @@
 package com.ciandt.summit.bootcamp2022.domain.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class MusicaDTO {
+
+    private String id;
+    private String nome;
+    private ArtistaDTO artista;
+
+    public Musica toMusica(){
+        return new Musica(getId(), getNome(), getArtista());
+    }
+
 }
