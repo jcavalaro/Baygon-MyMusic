eac2e11e760ebe016d1b0a168c22efb1d574dac6
CIANDT\igabriel <Igabriel@ciandt.com>
2022-07-28 16:03:31 -0300

    BOOT-458 criação classe teste MusicServiceImpl

--- MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/domain/services/MusicaServiceImplTest.java
+++ MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/domain/services/MusicaServiceImplTest.java
@@ -1,13 +1,21 @@
 package com.ciandt.summit.bootcamp2022.domain.services;
 
 import com.ciandt.summit.bootcamp2022.ApplicationConfigTest;
+import com.ciandt.summit.bootcamp2022.domain.dtos.ArtistaDTO;
+import com.ciandt.summit.bootcamp2022.domain.models.Musica;
 import com.ciandt.summit.bootcamp2022.domain.ports.repositories.MusicaRepositoryPort;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatcher;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 import org.mockito.Mockito.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.request;
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.when;
@@ -23,8 +31,10 @@ public class MusicaServiceImplTest extends ApplicationConfigTest {
     @DisplayName("deve trazer lista por filtro artista ou musica")
     public void TrazerListaPorFiltro(){
 
-        Mockito.when(musicaRepositoryPort.findByNameArtistaOrNameMusica("asdjasid")).then(request("spo"));
+        List<Musica> list = Collections.singletonList(Mockito.mock(Musica.class));
 
+        String filter = "u2";
+        Mockito.when(musicaRepositoryPort.findByNameArtistaOrNameMusica(ArgumentMatchers.eq(filter))).thenReturn(list);
     }
 
     @Test
