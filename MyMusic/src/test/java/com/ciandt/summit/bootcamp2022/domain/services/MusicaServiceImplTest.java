39065face8c2a9f10b56aa145163bf90448c3ecc
CIANDT\gabrielalichy <gabrielalichy@ciandt@gmail.com>
2022-07-28 16:58:39 -0300

    trazer todas as musicas

--- MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/domain/services/MusicaServiceImplTest.java
+++ MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/domain/services/MusicaServiceImplTest.java
@@ -1,13 +1,10 @@
 package com.ciandt.summit.bootcamp2022.domain.services;
 
 import com.ciandt.summit.bootcamp2022.ApplicationConfigTest;
-import com.ciandt.summit.bootcamp2022.domain.dtos.ArtistaDTO;
 import com.ciandt.summit.bootcamp2022.domain.models.Musica;
 import com.ciandt.summit.bootcamp2022.domain.ports.repositories.MusicaRepositoryPort;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatcher;
-import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 import org.mockito.Mockito.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +12,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.request;
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.when;
@@ -31,15 +27,17 @@ public class MusicaServiceImplTest extends ApplicationConfigTest {
     @DisplayName("deve trazer lista por filtro artista ou musica")
     public void TrazerListaPorFiltro(){
 
-        List<Musica> list = Collections.singletonList(Mockito.mock(Musica.class));
 
-        String filter = "u2";
-        Mockito.when(musicaRepositoryPort.findByNameArtistaOrNameMusica(ArgumentMatchers.eq(filter))).thenReturn(list);
     }
 
     @Test
     @DisplayName("deve trazer todas as musicas")
     public void TrazerTodasAsMusicas() {
+
+        String filter = " ";
+        List<Musica> list = Collections.singletonList(Mockito.mock(Musica.class));
+
+        Mockito.when(musicaRepositoryPort.findByNameArtistaOrNameMusica(filter)).thenReturn(list);
 
 
 
