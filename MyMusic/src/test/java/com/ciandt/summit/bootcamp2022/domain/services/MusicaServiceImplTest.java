9f221f5b090f1af25aca22bffedb3746c8e8c4d8
CIANDT\igabriel <Igabriel@ciandt.com>
2022-07-28 15:24:37 -0300

    BOOT-458 criação classe teste MusicServiceImpl

--- MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/domain/services/MusicaServiceImplTest.java
+++ MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/domain/services/MusicaServiceImplTest.java
@@ -1,8 +1,30 @@
 package com.ciandt.summit.bootcamp2022.domain.services;
 
 import com.ciandt.summit.bootcamp2022.ApplicationConfigTest;
+import com.ciandt.summit.bootcamp2022.domain.ports.repositories.MusicaRepositoryPort;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.Mockito.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.when;
 
 @DisplayName("MusicaServiceImplTest")
 public class MusicaServiceImplTest extends ApplicationConfigTest {
+
+
+    @MockBean
+    private MusicaRepositoryPort musicaRepositoryPort;
+
+    @Test
+    @DisplayName("deve trazer lista por filtro artista ou musica")
+    public void TrazerListaPorFiltro(){
+
+
+    }
+
+
+
 }
