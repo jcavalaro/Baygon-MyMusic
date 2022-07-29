edd435e1980e152cf92f31f96189689c46d94ecb
Cesar A. Pires <cesarp@ciandt.com>
2022-07-29 10:01:18 -0300

    feat: passando regra de negócio da controller para service

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/domain/services/MusicaServiceImpl.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/domain/services/MusicaServiceImpl.java
@@ -1,10 +1,17 @@
 package com.ciandt.summit.bootcamp2022.domain.services;
 
+import com.ciandt.summit.bootcamp2022.domain.dtos.DataDTO;
 import com.ciandt.summit.bootcamp2022.domain.dtos.MusicaDTO;
 import com.ciandt.summit.bootcamp2022.domain.models.Musica;
 import com.ciandt.summit.bootcamp2022.domain.ports.interfaces.MusicaServicePort;
 import com.ciandt.summit.bootcamp2022.domain.ports.repositories.MusicaRepositoryPort;
+import com.ciandt.summit.bootcamp2022.infrastructure.adapters.controllers.MusicaController;
+import com.ciandt.summit.bootcamp2022.infrastructure.adapters.controllers.exceptions.RuleLengthViolationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,12 +20,32 @@ import java.util.stream.Collectors;
 @Service
 public class MusicaServiceImpl implements MusicaServicePort {
 
+    private static final Logger logger = LoggerFactory.getLogger(MusicaController.class.getName());
+
     @Autowired
     private MusicaRepositoryPort musicaRepositoryPort;
 
     @Override
     public List<MusicaDTO> findByNameArtistaOrNameMusica(String name) {
+        if (name.isEmpty()) {
+            logger.info("Filtro não informado, retornando todas as musicas.");
+            return findAll();
+        }
+
+        if (name.length() < 2) {
+            logger.warn("Filtro informado com menos de 2 caracteres, lançando exceção.");
+            throw new RuleLengthViolationException();
+        }
+
         List<Musica> musicas = musicaRepositoryPort.findByNameArtistaOrNameMusica(name);
+
+        if (musicas.isEmpty()) {
+            logger.info("Musicas não encontradas com o filtro " + name);
+        } else {
+            logger.info("Foram encontradas " + musicas.size() + " musicas com o filtro " + name);
+        }
+
+
         return musicas.stream().map(Musica::toMusicaDTO).collect(Collectors.toList());
     }
 
