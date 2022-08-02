b574efeb315bc9a87a55a6c50a2752e998fb417b
CIANDT\gabrielalichy <gabrielalichy@ciandt@gmail.com>
2022-08-02 11:39:38 -0300

    feat: verificação do relacionamento das classes

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/domain/models/Playlist.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/domain/models/Playlist.java
@@ -1,9 +1,15 @@
 package com.ciandt.summit.bootcamp2022.domain.models;
 
+import com.ciandt.summit.bootcamp2022.domain.dtos.ArtistaDTO;
+import com.ciandt.summit.bootcamp2022.domain.dtos.MusicaDTO;
+import com.ciandt.summit.bootcamp2022.domain.dtos.PlaylistDTO;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -11,6 +17,17 @@ import lombok.Setter;
 @Setter
 public class Playlist {
 
-    private String id;
 
+    List<Musica> playlistMusic = new ArrayList<>();
+
+    private String id;
+    private String musicId;
+    private List<Musica> musicas = new ArrayList<>();
+
+    public Playlist(PlaylistDTO playlistDTO) {
+        playlistDTO.setId(id);
+        playlistDTO.setMusicId(musicId);
+        playlistDTO.setMusicas(playlistDTO.getMusicas());
+    }
 }
+
