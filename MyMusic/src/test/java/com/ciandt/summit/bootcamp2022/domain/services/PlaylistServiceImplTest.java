e7bc1425936ffdc6b329b1107567bf20477b8d44
tc-anaalves <anaalves@ciandt.com>
2022-08-03 12:22:15 -0300

    feat: criando teste findById da musicServiceImpl

--- MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/domain/services/PlaylistServiceImplTest.java
+++ MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/domain/services/PlaylistServiceImplTest.java
@@ -1,8 +1,25 @@
 package com.ciandt.summit.bootcamp2022.domain.services;
 
 import com.ciandt.summit.bootcamp2022.ApplicationConfigTest;
-import org.junit.jupiter.api.DisplayName;
+import com.ciandt.summit.bootcamp2022.domain.ports.repositories.PlaylistRepositoryPort;
+import org.junit.Before;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@DisplayName("PlaylistServiceImplTest")
+@SpringBootTest
 public class PlaylistServiceImplTest extends ApplicationConfigTest {
+
+    @Mock
+    PlaylistRepositoryPort playlistRepositoryPort;
+
+    @InjectMocks
+    PlaylistServiceImpl playlistServiceImpl;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
 }
