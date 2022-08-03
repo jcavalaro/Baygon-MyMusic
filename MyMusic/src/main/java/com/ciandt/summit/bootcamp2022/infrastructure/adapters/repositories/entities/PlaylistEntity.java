b1bb46839504cc19f00cf0f66c701b4a78dd625c
Gabriela Lichy <105449193+gabrielalichy@users.noreply.github.com>
2022-08-03 14:03:58 -0300

    Update PlaylistEntity.java

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/infrastructure/adapters/repositories/entities/PlaylistEntity.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/infrastructure/adapters/repositories/entities/PlaylistEntity.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.Hibernate;
-
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
