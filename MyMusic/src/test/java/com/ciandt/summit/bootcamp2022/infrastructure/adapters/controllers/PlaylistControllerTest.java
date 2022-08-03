6c7a169617e3e02605e373e10243954f237c7f7b
CIANDT\igabriel <Igabriel@ciandt.com>
2022-08-03 13:42:19 -0300

    BOOT-495 criando PlaylistControllerTest

--- MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/infrastructure/adapters/controllers/PlaylistControllerTest.java
+++ MyMusic/src/test/java/com/ciandt/summit/bootcamp2022/infrastructure/adapters/controllers/PlaylistControllerTest.java
@@ -1,8 +1,11 @@
 package com.ciandt.summit.bootcamp2022.infrastructure.adapters.controllers;
 
 import com.ciandt.summit.bootcamp2022.domain.dtos.ArtistDTO;
+import com.ciandt.summit.bootcamp2022.domain.dtos.DataDTO;
 import com.ciandt.summit.bootcamp2022.domain.dtos.MusicDTO;
+import com.ciandt.summit.bootcamp2022.domain.dtos.PlaylistDTO;
 import com.ciandt.summit.bootcamp2022.domain.services.MusicServiceImpl;
+import com.ciandt.summit.bootcamp2022.domain.services.PlaylistServiceImpl;
 import com.ciandt.summit.bootcamp2022.domain.services.exceptions.BusinessRuleException;
 import com.ciandt.summit.bootcamp2022.domain.services.exceptions.NotFoundException;
 import com.ciandt.summit.bootcamp2022.infrastructure.adapters.controllers.exceptions.ExceptionService;
@@ -33,6 +36,51 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(MockitoJUnitRunner.class)
 public class PlaylistControllerTest {
+
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Mock
+    PlaylistServiceImpl playlistService;
+
+    @InjectMocks
+    private PlaylistController playlistController;
+
+    final String id = "a39926f4-6acb-4497-884f-d4e5296ef652";
+
+    ArtistDTO artist1 = new ArtistDTO(UUID.randomUUID().toString(), "Bruno Mars");
+    MusicDTO music1 = new MusicDTO(UUID.randomUUID().toString(), "Talking to the moon", artist1);
+    ArtistDTO artist2 = new ArtistDTO(UUID.randomUUID().toString(), "The Beatles");
+    MusicDTO music2 = new MusicDTO(UUID.randomUUID().toString(), "Here Comes the Sun", artist2);
+    ArtistDTO artist3 = new ArtistDTO(UUID.randomUUID().toString(), "Michael Jackson");
+    MusicDTO music3 = new MusicDTO(UUID.randomUUID().toString(), "Billie Jean", artist3);
+
+    DataDTO dataDTORequest = new DataDTO(new ArrayList<>(Arrays.asList(music1,music2,music3)));
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(playlistController).setControllerAdvice(new ExceptionService()).build();
+    }
+
+    @Test
+    public void mustReturn200WhenListMusicIsAdd() throws Exception {
+        PlaylistDTO playlistDTO = new PlaylistDTO(id,new ArrayList<>(Arrays.asList(music1,music2,music3)));
+
+        when(playlistService.addMusicsToPlaylist(id, dataDTORequest)).thenReturn(playlistDTO);
+
+        String playlistId = id;
+        String uri = "/api/v1/playlists/{playlistId}/musicas";
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .post(uri,"playlistId")
+                        .param("playlistId",playlistId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+
+    }
 
 
 }
