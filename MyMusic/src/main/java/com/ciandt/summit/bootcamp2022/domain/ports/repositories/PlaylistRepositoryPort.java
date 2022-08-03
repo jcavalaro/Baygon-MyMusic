db6955c79534af3d6cea539b0c3fbe2a49db7f3c
Cesar A. Pires <cesarp@ciandt.com>
2022-08-03 09:42:48 -0300

    feat: adicionando camada de playlistRepository, playlistJPARepository e playlistRepositoryPort

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/domain/ports/repositories/PlaylistRepositoryPort.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/domain/ports/repositories/PlaylistRepositoryPort.java
@@ -1,4 +1,17 @@
 package com.ciandt.summit.bootcamp2022.domain.ports.repositories;
 
+import com.ciandt.summit.bootcamp2022.domain.models.Musica;
+import com.ciandt.summit.bootcamp2022.domain.models.Playlist;
+import com.ciandt.summit.bootcamp2022.infrastructure.adapters.repositories.entities.PlaylistEntity;
+
+import java.util.List;
+
 public interface PlaylistRepositoryPort {
+    Playlist findPlaylistById(String id);
+
+    List<Playlist>  findAll() ;
+
+    void addMusicRepository(PlaylistEntity playlist);
+
+
 }
--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/infrastructure/adapters/repositories/PlaylistRepository.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/infrastructure/adapters/repositories/PlaylistRepository.java
@@ -1,4 +1,34 @@
 package com.ciandt.summit.bootcamp2022.infrastructure.adapters.repositories;
 
-public class PlaylistRepository {
+import com.ciandt.summit.bootcamp2022.domain.models.Musica;
+import com.ciandt.summit.bootcamp2022.domain.models.Playlist;
+import com.ciandt.summit.bootcamp2022.domain.ports.repositories.PlaylistRepositoryPort;
+import com.ciandt.summit.bootcamp2022.infrastructure.adapters.repositories.entities.PlaylistEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class PlaylistRepository implements PlaylistRepositoryPort {
+
+    @Autowired
+    private PlaylistJpaRepository playlistJpaRepository;
+
+    @Override
+    public Playlist findPlaylistById(String id) {
+        Optional<PlaylistEntity> playlist = playlistJpaRepository.findById(id);
+        return playlist.get().toPlayList();
+    }
+
+    @Override
+    public List<Playlist> findAll() {
+        List<PlaylistEntity> playlist = playlistJpaRepository.findAll();
+        return playlist.stream().map(PlaylistEntity::toPlayList).collect(Collectors.toList());
+    }
+
+    @Override
+    public void addMusicRepository(PlaylistEntity playlist) {
+        playlistJpaRepository.save(playlist);
+    }
 }
